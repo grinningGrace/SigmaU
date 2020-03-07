1f799bd508de9964ac87a8b1bb02e3efe3bcf3b9
comprwwang163 <comprwwang@gmail.com>
2020-03-07 13:33:31 +0800

    listview update

--- CoursePage.py
+++ CoursePage.py
@@ -1,5 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton
+
+from PyQt5.QtCore import QStringListModel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QListView, QMessageBox
 import MainPage
 import deleteCoursePage
 from PyQt5 import QtCore, QtGui, QtWidgets
@@ -10,7 +12,20 @@ class CoursePage(QtWidgets.QDialog,Ui_Dialog):
     def __init__(self):
         super(CoursePage, self).__init__()
         # self.init_ui(self)
+
         self.setupUi(self)
+
+
+        l = ['IEI 1001', 'IEI 1002', 'IEI 1008','CBWA', 'MSM' ,'BSDA','BDA']
+        slm = QStringListModel()
+        self.listView.qList = l
+
+        slm.setStringList(l)
+        self.listView.setModel(slm)
+        listView = QListView()
+        listView.setModel(slm)
+
+        self.listView.clicked.connect(self.CourseItemClicked)
         self.addCourse.clicked.connect(self.addCourse_btn_click)
         self.deleteCourse.clicked.connect(self.deleteCourse_btn_click)
         self.logOut.clicked.connect(self.logOut_btn_click)
@@ -32,3 +47,7 @@ class CoursePage(QtWidgets.QDialog,Ui_Dialog):
         self.hide()
         self.s = deleteCoursePage.deleteCousePage()
         self.s.show()
+
+
+    def CourseItemClicked(self, qModelIndex):
+        QMessageBox.information(self,'ListWidget','你选择了：'+self.listView.qList[qModelIndex.row()])
